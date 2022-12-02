9e2693eaec0d251f644d2ea7eefdebef98bded6b
Nazar Shtonda <nazarshtonda@MacBook-Air-Nazar.local>
2022-12-02 11:56:06 +0200

    ss2

---
+++ scrip1t.js
@@ -0,0 +1,25 @@
+"use strict"
+
+let num = 50;
+while (num <= 55) {
+    num++
+    console.log(num);
+};
+
+
+let num = 50;
+
+do {
+    num++
+    console.log(num);
+}
+
+while (num < 55); 
+
+for (let i = 1; i < 4; i++) {
+
+    console.log(i)
+
+}
+
+ 
